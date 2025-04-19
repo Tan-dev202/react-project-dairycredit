e20067b56f1fb76fa5ec9d8418dde0bf1bb40e62
Enock Tangus <tanguskipngeno@gmail.com>
2025-04-19 21:23:34 +0300

    wrap routerprovider in darkmodeprovider

--- src/index.js
+++ src/index.js
@@ -4,19 +4,41 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPageLayout from './layouts/LandingPageLayout';
+import { DarkModeProvider } from "./context/DarkModeContext";
 import About from "./components/About";
 import Faqs from "./components/Faqs";
 import Contact from "./components/Contact";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/about", element: <About /> },
-  { path: "/faqs", element: <Faqs /> },
-  { path: "/contact", element: <Contact /> },
+  {
+    path: '/',
+    element: <LandingPageLayout />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      { path: "/faqs", 
+        element: <Faqs /> 
+      },
+      { path: "/contact", 
+        element: <Contact /> 
+      }
+    ],
+  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(<React.StrictMode>
+  <DarkModeProvider>
+    <RouterProvider router={router} />
+  </DarkModeProvider>
+</React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
