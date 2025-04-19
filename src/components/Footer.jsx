51e0e48361ace4f3ab130a0146145ef65dde2356
Enock Tangus <tanguskipngeno@gmail.com>
2025-04-19 21:12:35 +0300

    remove fixed class

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="footer bg-dark text-white py-4 mt-5">
+    <footer className="footer bg-dark text-white">
       <div className="container text-center">
         &copy; {currentYear} DairyCredit | All Rights Reserved
       </div>
